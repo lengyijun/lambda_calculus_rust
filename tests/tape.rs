3e30e4e1867e11cc11dc905716e122d5a1475f4f
lengyijun <sjtu5140809011@gmail.com>
2025-02-07 17:56:20 +0800

    test fail

--- tests/tape.rs
+++ tests/tape.rs
@@ -2,6 +2,8 @@
 
 extern crate lambda_calculus;
 
+use lambda_calculus::data::boolean::fls;
+use lambda_calculus::data::boolean::tru;
 use lambda_calculus::data::list::church::nil;
 use lambda_calculus::data::turing_machine::tape::*;
 use lambda_calculus::*;
@@ -13,16 +15,10 @@ fn test_mmove() {
     assert_eq!(beta(app!(pi!(3, 3), new_tape()), HSP, 0), nil());
     // let x = app!(move_left(), new_tape());
     let x = app!(write(), 0.into_church(), new_tape());
-    assert_eq!(
-        beta(app!(pi!(2, 3), x.clone()), HSP, 0),
-        beta(app!(pi!(2, 3), new_tape()), HSP, 0),
-    );
-    /*
-    assert_eq!(
-        beta(app!(pi!(3, 3), x), HSP, 0),
-        beta(app!(pi!(3, 3), new_tape()), HSP, 0),
-    );
-     */
+    assert_eq!(beta(app!(pi!(2, 3), x.clone()), HSP, 0), tru(),);
+
+    assert_eq!(beta(app!(pi!(3, 3), x.clone()), HSP, 0), fls());
+    assert_eq!(beta(app!(pi!(1, 3), x.clone()), HSP, 0), 0.into_church());
     // assert_eq!(beta(app!(move_right(), new_tape()), HSP, 0), new_tape());
 }
 
